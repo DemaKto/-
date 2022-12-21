9815dd5d797c041c247c3dad1fd5a77b873b9ee8
DemaKto <121171105+DemaKto@users.noreply.github.com>
2022-12-21 19:01:27 +0500

    Update main.cpp

--- р1з3/main.cpp
+++ р1з3/main.cpp
@@ -1,30 +1,30 @@
-# включить  < iostream >
-# включить  < math.h >
-// #include <stdlib.h>
-используя  пространство имен  std ;
-основной  ( )
+#include <iostream>
+#include <math.h>
+//#include <stdlib.h>
+using namespace std;
+int main ()
 {
-    int ха, ya, xb, yb, xc, yc;
+    int xa,ya,xb,yb,xc,yc;
     float ab,bc,ac,p,s,r,length,scircle;
-    cout << " введите Xa, Ya " <<endl;
-    цин >> ха >> я;
-    cout << " введите Xb, Yb " <<endl;
+    cout<<"enter Xa, Ya"<<endl;
+    cin>>xa>>ya;
+    cout<<"enter Xb, Yb"<<endl;
     cin>>xb>>yb;
-    cout << " введите Xc, Yc " <<endl;
+    cout<<"enter Xc, Yc"<<endl;
     cin>>xc>>yc;
 
-    ab= sqrt ( pow (xb-xa, 2 )+ pow (yb-ya, 2 ));
-    bc= sqrt ( pow (xc-xb, 2 )+ pow (yc-yb, 2 ));
-    ac= sqrt ( pow (xc-xa, 2 )+ pow (yc-ya, 2 ));
-    р=аб+бк+ас;
-    s= sqrt (p/ 2 *(p/ 2 -ab)*(p/ 2 -bc)*(p/ 2 -ac));
-            // cout<<"Периметр треугольника, P= "<<p<<endl;
-            // cout<<"Площадь треугольника S= "<<s<<endl;
-    // r радиус вписанной окружности, равная площадь треугольника раздел на полупериметр
-    г=s/(р/ 2 );
-    длина= 2 *M_PI*r;
-    круг=M_PI* pow (r, 2 );
-    cout<< " Длина окружности l= " <<length<<endl;
-    cout<< " Площадь круга S= " <<scircle;
+    ab=sqrt(pow(xb-xa,2)+pow(yb-ya,2));
+    bc=sqrt(pow(xc-xb,2)+pow(yc-yb,2));
+    ac=sqrt(pow(xc-xa,2)+pow(yc-ya,2));
+    p=ab+bc+ac;
+    s=sqrt(p/2*(p/2-ab)*(p/2-bc)*(p/2-ac));
+            // cout<<"The perimeter of the triangle, P= "<<p<<endl;
+            // cout<<"The area of the triangle is S= "<<s<<endl;
+    //r радиус вписанной окружности, равен площадь треугольника разделить на полупериметр
+    r=s/(p/2);
+    length=2*M_PI*r;
+    scircle=M_PI*pow(r,2); 
+    cout<<"Length of circle l= "<<length<<endl;
+    cout<<"Area of circle S= "<<scircle;
 
 }
