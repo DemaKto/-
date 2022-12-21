fb71fa3014cf2b8c9e3523b0b968b6c935d563a8
DemaKto <121171105+DemaKto@users.noreply.github.com>
2022-12-21 19:04:42 +0500

    Update main.cpp

--- р2з3/main.cpp
+++ р2з3/main.cpp
@@ -1,35 +1,35 @@
-# включить  < iostream >
-# включить  < math.h >
-# включить  < iomanip >
-// #include <stdlib.h>
-используя  пространство имен  std ;
-основной  ( )
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+//#include <stdlib.h>
+using namespace std;
+int main ()
 {
-    инт н;
-    поплавок p,lsum,ltape;
-    р= 0 ;lsum= 0 ;
-    cout<< " введите n деревьев " <<endl;
-    цин>>н;
-    если (n< 2 ) { возврат  0 ;};
-    число с плавающей запятой x1, y1 , xa, ya, xb, yb;
-cout << " введите X1, Y1 " <<endl;
-    cin>>x1>> y1 ;
-    ха=х1; я = у1 ;
-    for ( int i= 1 ;i<n;i++) {
-        cout<< " введите X " <<i+ 1 << " , Y " <<i+ 1 <<endl;
+    int n;
+    float p,lsum,ltape;
+    p=0;lsum=0;
+    cout<<"enter n of trees"<<endl;
+    cin>>n;
+    if (n<2) {return 0;};
+    float x1,y1,xa,ya,xb,yb;
+ cout<<"enter X1, Y1"<<endl;
+    cin>>x1>>y1;
+    xa=x1; ya=y1;
+    for (int i=1;i<n;i++) {
+        cout<<"enter X"<<i+1<<", Y"<<i+1<<endl;
         cin>>xb>>yb;
-        если ((i==(n - 1 ))&&(n> 2 )) p+= sqrt ( pow (x1-xb, 2 )+ pow ( y1 -yb, 2 ));
-        p+= sqrt ( pow (xb-xa, 2 )+ pow (yb-ya, 2 ));
-        ха=хб; я=ыб;
-        }
-    для ( int i= 0 ; i<n; i++)
-    {    поплавок л;
-        cout << " войти в дерево l " <<i+ 1 <<endl;
-        цин >> л;
-        лсум+=1;
+        if ((i==(n-1))&&(n>2)) p+=sqrt(pow(x1-xb,2)+pow(y1-yb,2));
+        p+=sqrt(pow(xb-xa,2)+pow(yb-ya,2));
+        xa=xb; ya=yb;
+        } 
+    for (int i=0; i<n; i++)
+    {   float l;
+        cout<<"enter tree l"<<i+1<<endl;
+        cin>>l;
+        lsum+=l;
     }
     ltape=(lsum/n)+p;
-cout<< " p= " <<p<<endl; // /периметр многоугольника
-cout<< " l of tape = " <<fixed<< setprecision ( 2 )<<ltape; // длина ленты
+cout<<"p="<<p<<endl; ///периметр многоугольника
+cout<<"l of tape ="<<fixed<<setprecision(2)<<ltape; //длина ленты
 
 }
